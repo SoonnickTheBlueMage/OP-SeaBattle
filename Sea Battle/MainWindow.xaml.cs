ab018a5e34f1af9d69f5105d63eac62601c10238
Андриянов Александр <alexandri2003@gmail.com>
2022-10-07 02:54:49 +0300

    Добавил поле

--- Sea Battle/MainWindow.xaml.cs
+++ Sea Battle/MainWindow.xaml.cs
@@ -23,6 +23,63 @@ namespace Sea_Battle
         public MainWindow()
         {
             InitializeComponent();
+            
+            DrawTable();
+        }
+
+        private void DrawTable()
+        {
+            for (var rowIterator = 0; rowIterator < 11; rowIterator++)
+            for (var columnIterator = 0; columnIterator < 21; columnIterator++)
+            {
+                if (columnIterator == 10 && rowIterator != 10)
+                {
+                    var numberField = new TextBlock
+                    {
+                        Background = Brushes.White,
+                        Text = $"{rowIterator + 1}",
+                        FontSize = 17,
+                        TextAlignment = TextAlignment.Center
+                    };
+                    
+                    Grid.SetColumn(numberField, columnIterator);
+                    Grid.SetRow(numberField, rowIterator);
+
+                    GridDuoTable.Children.Add(numberField);
+                    continue;
+                }
+
+                if (rowIterator == 10 && columnIterator != 10)
+                {
+                    var letterField = new TextBlock
+                    {
+                        Background = Brushes.White,
+                        Text = $"{(char) (columnIterator % 11 + 'a')}",
+                        FontSize = 17,
+                        TextAlignment = TextAlignment.Center
+                    };
+                    
+                    Grid.SetColumn(letterField, columnIterator);
+                    Grid.SetRow(letterField, rowIterator);
+
+                    GridDuoTable.Children.Add(letterField);
+                    continue;
+                }
+
+                if (rowIterator == 10 && columnIterator == 10) continue;
+
+                var cell = new Border
+                {
+                    Background = Brushes.White,
+                    BorderBrush = Brushes.CornflowerBlue,
+                    BorderThickness = new Thickness(1)
+                };
+
+                Grid.SetColumn(cell, columnIterator);
+                Grid.SetRow(cell, rowIterator);
+
+                GridDuoTable.Children.Add(cell);
+            }
         }
     }
 }
